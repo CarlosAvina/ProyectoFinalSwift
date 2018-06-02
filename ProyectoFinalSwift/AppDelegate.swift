7ae0da3f8c239a3b12df25b3dfe5a5931d1831d9
Aviña <carlosavilu11@gmail.com>
2018-06-02 10:36:00 -0500

    Initial Commit

---
+++ ProyectoFinalSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProyectoFinalSwift
+//
+//  Created by ISSC411 on 02/06/18.
+//  Copyright © 2018 ISSC411. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProyectoFinalSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProyectoFinalSwift
+//
+//  Created by ISSC411 on 02/06/18.
+//  Copyright © 2018 ISSC411. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
