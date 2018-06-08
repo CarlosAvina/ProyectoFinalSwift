221d8f4e16d05c61a0f7c472c356d1584310456d
Dasan93 <mochiz123>
2018-06-08 15:50:17 -0500

    aksj∂µ≥ ≤

--- ProyectoFinalSwift/ViewController.swift
+++ ProyectoFinalSwift/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
         
-        //puto github y shaba
+        //puto github y ramon
     }
 
 
